049a1e9e0f797c8fd0c5eaab03a48068b641a855
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-01 14:17:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,13 +3,13 @@ char carattere(char carattere){
 
   if(carattere >= 'A' && carattere <= 'Z')
   {
-    int ris = carattere + 32;
+    char ris = carattere + 32;
     
     return ris;
   }
   else if(carattere >= 'a' && carattere <= 'z')
   {
-    int ris = carattere - 32;
+    char ris = carattere - 32;
     
     return ris;
   }
