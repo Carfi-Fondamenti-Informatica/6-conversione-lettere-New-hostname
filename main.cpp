ba4c73216012351437e2cc1d241aa79ae71a2c24
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-01 14:05:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+using namespace std;
 
 int main(){
+  
+  char carattere;
+  
+  cin >> carattere;
+  
+  conversione(carattere);
+  
+  if(char)
+  {
+    cout << conversione(carattere) << endl;
+  }
+  else
+  {
+    cout << "errore" << endl;
+  }
+  
   return 0;
 }
